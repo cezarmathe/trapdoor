c488ec7c04a2fe0bfb651433dd9918548e6b7cde
Armand-Cezar Mathe <me@cezarmathe.com>
2021-08-09 19:10:21 +0300

    Add basic `insert` method

--- src/raw.rs
+++ src/raw.rs
@@ -1,9 +1,21 @@
 //! Raw trapdoor.
 
-use std::sync::RwLock;
 use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering as AtomicOrdering;
+
+use parking_lot::RwLock;
 
 pub struct RawTrapdoor<T> {
     inner: [RwLock<Vec<T>>; 2],
     selector: AtomicUsize,
+}
+
+impl<T> RawTrapdoor<T> {
+    pub fn insert(&self, value: T)  {
+        let mut guard = {
+            let selector = self.selector.load(AtomicOrdering::Acquire);
+            self.inner[selector].write()
+        };
+        guard.push(value);
+    }
 }
