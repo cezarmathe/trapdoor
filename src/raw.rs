e7cdb498282d276cc959fcc1a02a8af1d2cce5d2
Armand-Cezar Mathe <me@cezarmathe.com>
2021-08-17 21:23:10 +0300

    Implement `take` and `insert`

--- src/raw.rs
+++ src/raw.rs
@@ -1,8 +1,10 @@
 //! Raw trapdoor.
 
+use std::mem;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering as AtomicOrdering;
 
+use parking_lot::Mutex;
 use parking_lot::RwLock;
 
 pub struct RawTrapdoor<T> {
@@ -24,11 +26,50 @@ impl<T> RawTrapdoor<T> {
         }
     }
 
-    pub fn insert(&self, value: T)  {
-        let mut guard = {
-            let selector = self.selector.load(AtomicOrdering::Acquire);
-            self.inner[selector].write()
-        };
-        guard.push(value);
+    /// Insert a value in the trapdoor.
+    pub fn insert(&self, value: T) {
+        loop {
+            let mut guard = {
+                let selector = self.get_selector();
+                self.inner[selector].write()
+            };
+            if let Some(collection) = &mut *guard {
+                collection.push(value);
+                break;
+            }
+        }
+    }
+
+    /// Take the collected data.
+    pub fn take(&self) -> Vec<T> {
+        let _ = self.take_lock.lock();
+        loop {
+            let collection = {
+                let selector = self.get_selector();
+                let mut guard = self.inner[selector].write();
+                mem::take(&mut *guard)
+            };
+            if let Some(value) = collection {
+                {
+                    let selector = self.switch_selector();
+                    let mut guard = self.inner[selector ^ 1].write();
+                    assert!(guard.is_none());
+                    *guard = Some(Vec::new());
+                }
+                break value;
+            }
+        }
+    }
+
+    /// Get the current selector.
+    #[inline(always)]
+    fn get_selector(&self) -> usize {
+        self.selector.load(AtomicOrdering::Acquire)
+    }
+
+    /// Switch the selector, returning the old value.
+    #[inline(always)]
+    fn switch_selector(&self) -> usize {
+        self.selector.fetch_xor(1, AtomicOrdering::SeqCst)
     }
 }
